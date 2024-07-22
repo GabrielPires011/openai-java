94f3d1fbc8dca13fd0f24cdda578fdd317fef83b
GabrielPires011 <pires.rocha011@gmail.com>
2024-07-22 15:23:04 -0300

    alter CatagorizadorDeProdutos

--- src/main/java/br/com/alura/CatagorizadorDeProdutos.java
+++ src/main/java/br/com/alura/CatagorizadorDeProdutos.java
@@ -5,6 +5,7 @@ import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.completion.chat.ChatMessageRole;
 import com.theokanning.openai.service.OpenAiService;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -14,13 +15,27 @@ public class CatagorizadorDeProdutos {
 
         var scarnner = new Scanner(System.in);
 
-        var system = "Você é um categorizador de produtos";
+        var system = """
+        Você é um categorizador de produtos e deve responder apenas o nome da categoria do produto informado
+        
+        Escolha uma categoria dentra a lista abaixo:
+        
+        1. Higiene pessoal
+        2. Eletronicos
+        3. Esportes
+        4. Outros
+        
+        ##### exemplo de uso:
+        
+        Pergunta: Bola de futebol
+        Resposta: Esportes
+        """;
 
         System.out.print("Informe seu produto: ");
         var user = scarnner.nextLine();
 
         var chave = System.getenv("OPENAI_API_KEY");
-        var service = new OpenAiService(chave);
+        var service = new OpenAiService(chave, Duration.ofSeconds(30));
 
         var completionRequest = ChatCompletionRequest
                 .builder()
